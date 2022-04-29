ba97ad1f2abc3bf26057d6a0f550f4a35e75d103
bebetterbos <bos34153@gmail.com>
2022-04-30 04:16:17 +0700

    commit

---
+++ src/components/CreateComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!-- Content goes here -->
+            <h1>Create Student</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!-- Display student list -->
+            <h1>Show Student</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
