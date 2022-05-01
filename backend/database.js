9e52e4d95bcafc95634a8e1633b8f1861451b72b
bebetterbos <bos34153@gmail.com>
2022-05-02 03:01:00 +0700

    commit

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb://admin:FOXftq81064@node31864-env-0806280.app.ruk-com.cloud:11267/subject'
+    db: 'mongodb://admin:FOXftq81064@node31864-env-0806280.app.ruk-com.cloud:11267'
 }
