f1ac871fee8db17de7f7abe17a12bd65449240e4
bebetterbos <bos34153@gmail.com>
2022-05-04 02:22:47 +0700

    commit

--- src/components/register.vue
+++ src/components/register.vue
@@ -48,7 +48,7 @@ export default {
             let apiURL = 'http://localhost:4000/api/create-user';
 
             axios.post(apiURL, this.users).then(() => {
-                this.$router.push('/');
+                this.$router.push('/login');
                 this.users = {
                     username: '',
                     password: '',
